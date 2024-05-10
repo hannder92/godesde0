5adf9141dff3981d8410971b6b46f360a30e766d
Johann Medina <hannder92@gmail.com>
2024-05-10 13:55:43 -0500

    2do Commit

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/hannder92/godesde0/variables"
+import (
+	"fmt"
+	"github.com/hannder92/godesde0/variables"
+)
 
 func main() {
 	variables.MostrarEnteros()
+	variables.RestoVariables()
+	estado, texto := variables.ConvertirATexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
